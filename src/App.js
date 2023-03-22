e86c751d520ca270d9b5537274ddf3eade3dc3f2
Anthony Croft <ajcsignups@gmail.com>
2023-03-22 17:02:38 +0000

    Created ratecards for the Index page and added added code to Index.js and App.js so they can be rendered.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from "react";
 // 1. [X ] App
 // 2. [ X] Header
-import Fun from './pages/Index';
+import Index from './pages/Index';
 import Business from './pages/Business';
 import Kids from './pages/Kids';
 import Header from './components/Header';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 import PackageCard from './components/PackageCard';
 
@@ -21,6 +22,33 @@ import PackageCard from './components/PackageCard';
 const title = "Welcome to the Home Page";
 const siteTitle = "Welcome to the Fun Page";
 
+const rateCards = [
+  { id: 1,
+    title: 'Package "Apéritif"',
+    subTitle: 'Free 30-minute trial',
+    description: 'Short and sweet - book your free 30-minute trial and start speaking French like the French. Salutations!',
+    rate: 'Free',
+    btnMsg: 'Book Now',
+    link: "https://calendly.com/teamcarambatesting/1-2-1-tutoring-session-free-trial"
+  },
+  { id: 2,
+    title: 'Package "Crêpe"',
+    subTitle: 'Pay as you go',
+    description: 'Not sure how much time you can commit to learning French? Often out of town? Choose the flexible "pay as you go" option to only book classes that fit your busy schedule.',
+    rate: '£35/hour',
+    btnMsg: 'Get in touch',
+    link: "https://form.jotform.com/230464330195350"
+  },
+  { id: 3,
+    title: 'Best Value',
+    subTitle: 'Package "Ratatouille"',
+    description: 'Looking for regular weekly classes to speak French as quickly as possible? Opt for the monthly subscription and book 8 hourly classes that can fit within your schedule.',
+    rate: '£200/month (8 hours)',
+    btnMsg: 'Get in touch',
+    link: "https://form.jotform.com/230464330195350"
+  }
+];
+
 function App() {
 
   return (
@@ -29,7 +57,7 @@ function App() {
         <div>
             <Header siteTitle={siteTitle}/>
             <Routes>
-              <Route path="/" element={<Fun title={title}/>} />
+              <Route path="/" element={<Index rateCards={rateCards} />} />
               {/* <Route path="/business" element={<Contact contactTitle1={contactTitle1} contactTitle2={contactTitle2} email={email}/>} />
               <Route path="/kids" element={<Kids/>} />
               <Route path="*" element={<NotFound/>} /> */}
--- src/components/PackageCard.js
+++ src/components/PackageCard.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './PackageCard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const PackageCard = ({ props }) => {
+const PackageCard = ({ title, subTitle, description, rate, btnMsg, link }) => {
+
+
   return (
 
     <div className="card-wrapper">
       <div className="card">
-        {/* <div className="chart_best_value">{props.best-value}</div>
-        <div className="card-body card-body-3">
-          <h5 className="card-title">{props.card-title}</h5>
-          <h6 class="card-subtitle">{props.card-subtitle}</h6>
-          <p className="card-text">
-            {props.card-text}
+        <div className="chart_best_value"></div>
+        <div className="card-body">
+          <h5 className="card-title text-center mb-2">{title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted text-center">{subTitle}</h6>
+          <p className="card-text text-center">
+            {description}
           </p>
-          <p className="card-text-secondary">{props.card-subtitle}</p>
+          <p className="card-text text-center">{rate}</p>
           <div className="text-center">
-              <button type="button" className="btn btn-secondary btn-sm"><a href="https://form.jotform.com/230464330195350">{props.btnMessage}</a></button>
+              <button type="button" className="btn btn-secondary btn-sm button-font"><a href={link}>{btnMsg}</a></button>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
 
