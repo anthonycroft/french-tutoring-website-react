5be09b9550a16d9145faeef82ca15b37d4a1c884
Anthony Croft <ajcsignups@gmail.com>
2023-03-24 20:06:29 +0000

    Fix issue with Calendly not rendering correctly.

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -13,7 +13,7 @@ const Calendly = () => {
   }, []);
 
   return (
-    <div className="calendly-container col-lg-6 col-md-12 col-sm-12 text-center d-flex align-items-center">
+    <div>
       <div
         className="calendly-inline-widget"
         data-url="https://calendly.com/teamcarambatesting/1-2-1-tutoring-session-free-trial-parents"
