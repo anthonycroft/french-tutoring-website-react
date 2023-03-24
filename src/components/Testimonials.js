292ef6b4879bdd69fbee750523aa4216438f7e71
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-03-24 09:26:42 +0000

    updated VocalVideo component and Testimonials.js imports, VocalVideo component now embedded in Testimonials component. Also updated package.json with installed dependencies, react-slick and slick-carousel

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,15 +1,14 @@
 import React from "react";
 import './Testimonials.css';
-import Bertie from '../assets/images/Bertrand-Icon.png';
-import VideoEmbed from "./VideoEmbed";
+import VocalVideo from "./VocalVideo.js";
 function Testimonials() {
-  console.log(Bertie);
+ 
   return (
     <section className="jumbotron testimonials" id="testimonials">
       <div className="container">
         <h1 className="display-4 text-center test-video section-heading">Testimonials</h1>
 
-    <VideoEmbed/>
+    <VocalVideo/>
 
       </div>
     </section>
